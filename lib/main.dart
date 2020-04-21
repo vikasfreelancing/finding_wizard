3a0e83f0fcd5feb8884344f2c29f5a99f7acc51b
pankajsharma <yellow*99>
2020-04-21 16:29:50 +0530

    adding messanger to my app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:lost_and_found/screens/lostItemViewList.dart';
 import 'home.dart';
 import 'screens/login.dart';
 import 'screens/register.dart';
+import 'messagingModule/screens/welcome_screen.dart';
+import 'messagingModule/screens/login_screen.dart';
+import 'messagingModule/screens/registration_screen.dart';
+import 'messagingModule/screens/chat_screen.dart';
 
 void main() => runApp(LostAndFoundApp());
 
@@ -15,13 +19,17 @@ class LostAndFoundApp extends StatelessWidget {
         primaryColor: Color(0xFF6D60FB),
         scaffoldBackgroundColor: Color(0xFF6D60FB),
       ),
-      initialRoute: "/",
+      initialRoute: WelcomeScreen.id,
       routes: {
         "/": (context) => Home(),
         "/login": (context) => LogIn(),
         "/registor": (context) => Register(),
         "/newItem": (context) => LostItem(),
         "/listItems": (context) => LostItemView(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
       },
     );
   }
